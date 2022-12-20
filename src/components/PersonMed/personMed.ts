5ea5006ba65c75f05384c205fab7fd2db9f9b6c0
Ivan Biryukov <vanyabiryukov@yandex.ru>
2022-12-20 22:58:54 +0300

    fix personMed css

--- src/components/PersonMed/personMed.ts
+++ src/components/PersonMed/personMed.ts
@@ -1,7 +1,7 @@
 import template from '@components/PersonMed/personMed.handlebars';
 import { Component } from '@components/Component';
 import '@/index.scss';
-import '@components/Userbar/userbar.scss';
+import '@components/PersonMed/personMed.scss';
 
 export class PersonMedUI extends Component {
     static renderTemplate(args:any) {
