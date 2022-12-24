384f0e06978be58a9a6381b1d950383ea25a9745
Mike5535 <volkovnhk@mail.ru>
2022-12-24 13:46:53 +0300

    add examples

--- src/components/PersonMed/personMed.stories.ts
+++ src/components/PersonMed/personMed.stories.ts
@@ -5,10 +5,18 @@ export default {
     component: PersonMedUI,
 };
 
-export const Default = (args:any) => {
+export const Default = (args: any) => {
     return PersonMedUI.renderTemplate(args);
 };
 
 Default.args = {
-    props: {},
+    name: 'Кристиан Бэйл',
+    professions: [
+        'актер',
+        'продюссер',
+    ],
+    age: '33',
+    count_films: '130',
+    avatar: "https://uznayvse.ru/images/catalog/2022/2/christian-bale_26.jpg",
+    id: "1",
 };
